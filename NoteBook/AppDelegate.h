2adfe382e62a72882a83aaf76c4aa0274f9359ed
Windream <lei_dream@sina.cn>
2015-04-22 12:40:45 +0800

    Initial Commit

---
+++ NoteBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoteBook
+//
+//  Created by 孟雷 on 15/4/22.
+//  Copyright (c) 2015年 windream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NoteBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoteBook
+//
+//  Created by 孟雷 on 15/4/22.
+//  Copyright (c) 2015年 windream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
